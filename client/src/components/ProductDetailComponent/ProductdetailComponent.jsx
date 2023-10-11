95779368ead44ca85a4b6ff95afdb20efa78811a
minhnhut1712 <nminhnhut0112@gmail.com>
2023-10-12 00:30:54 +0700

    gird layout

--- client/src/components/ProductDetailComponent/ProductdetailComponent.jsx
+++ client/src/components/ProductDetailComponent/ProductdetailComponent.jsx
@@ -22,7 +22,14 @@ const ProductdetailComponent = () => {
       <Grid
         container
         spacing={3}
-        sx={{ padding: { xs: "0 20px", md: "20px 50px" } }}
+        sx={{
+          padding: {
+            xs: "0 20px",
+            md: "20px 50px",
+            display: "flex",
+            justifyContent: "center",
+          },
+        }}
         className="row"
       >
         <Grid item xs={12} md={7} spacing={2} container className="row__col">
@@ -36,12 +43,12 @@ const ProductdetailComponent = () => {
             <img src={product1} alt="1" id="imgbig" />
           </Grid>
         </Grid>
-        <Grid item xs={12} md={5} className="row__content">
+        <Grid item xs={12} md={3} className="row__content">
           <p className="row__content__title">
             NUKE 26245 <br />
             <span className="row__content__category">Jordan</span>
           </p>
-          <p className="row__content__price">45$</p>
+          <p className="row__content__price">$ 45</p>
           <div className="row__content__selectsize">
             <p>Select Size</p>
             <div>
@@ -50,24 +57,44 @@ const ProductdetailComponent = () => {
           </div>
 
           <div className="row__content__size">
-            <input type="radio" id="40" name="fav_language" value="40" />
-            <label htmlFor="40">40</label>
-            <input type="radio" id="41" name="fav_language" value="41" />
-            <label htmlFor="41">41</label>
-            <input type="radio" id="42" name="fav_language" value="42" />
-            <label htmlFor="42">42</label>
-            <input type="radio" id="43" name="fav_language" value="43" />
-            <label htmlFor="43">43</label>
-            <input type="radio" id="44" name="fav_language" value="44" />
-            <label htmlFor="44">44</label>
-            <input type="radio" id="45" name="fav_language" value="45" />
-            <label htmlFor="45">45</label>
-            <input type="radio" id="46" name="fav_language" value="46" />
-            <label htmlFor="46">46</label>
-            <input type="radio" id="47" name="fav_language" value="47" />
-            <label htmlFor="47">47</label>
-            <input type="radio" id="48" name="fav_language" value="48" />
-            <label htmlFor="48">48</label>
+            <Grid container spacing={2}>
+              <Grid item xs={3}>
+                <input type="radio" id="40" name="size" value="40" />
+                <label htmlFor="40">40</label>
+              </Grid>
+              <Grid item xs={3}>
+                <input type="radio" id="41" name="size" value="41" />
+                <label htmlFor="41">41</label>
+              </Grid>
+              <Grid item xs={3}>
+                <input type="radio" id="42" name="size" value="42" />
+                <label htmlFor="42">42</label>
+              </Grid>
+              <Grid item xs={3}>
+                <input type="radio" id="43" name="size" value="43" />
+                <label htmlFor="43">43</label>
+              </Grid>
+              <Grid item xs={3}>
+                <input type="radio" id="44" name="size" value="44" />
+                <label htmlFor="44">44</label>
+              </Grid>
+              <Grid item xs={3}>
+                <input type="radio" id="45" name="size" value="45" />
+                <label htmlFor="45">45</label>
+              </Grid>
+              <Grid item xs={3}>
+                <input type="radio" id="46" name="size" value="46" />
+                <label htmlFor="46">46</label>
+              </Grid>
+              <Grid item xs={3}>
+                <input type="radio" id="47" name="size" value="47" />
+                <label htmlFor="47">47</label>
+              </Grid>
+              <Grid item xs={3}>
+                <input type="radio" id="48" name="size" value="48" />
+                <label htmlFor="48">48</label>
+              </Grid>
+            </Grid>
           </div>
           <button className="row__content__addcart">add to card</button>
         </Grid>
